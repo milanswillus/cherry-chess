4e3688caee4511c301bc8aaab0581f3ebd644bd7
Milan Swillus <mil4nmil4n@icloud.com>
2026-05-12 21:20:01 +0200

    Initial Commit

---
+++ ChessAnalyzer/ChessAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessAnalyzerApp.swift
+//  ChessAnalyzer
+//
+//  Created by milxn on 12.05.26.
+//
+
+import SwiftUI
+
+@main
+struct ChessAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
